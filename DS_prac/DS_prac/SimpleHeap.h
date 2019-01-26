1d2270e95e6c2d00b4508a09f0c82392af0984e1
Hyeonseong Jeon <cutz309@gmail.com>
2019-01-26 18:23:37 +0900

    Update

---
+++ DS_prac/DS_prac/SimpleHeap.h
@@ -0,0 +1,26 @@
+#ifndef __SIMPLE_HEAP_H__
+#define __SIMPLE_HEAP_H__
+
+#define TRUE 0
+#define FALSE 1
+#define HEAP_LEN 100
+
+typedef char HData;
+typedef int Priority;
+typedef struct _heapElem {
+	Priority pr;
+	HData data;
+} HeapElem;
+
+typedef struct _heap {
+	int numofData;
+	HeapElem heapArr[HEAP_LEN];
+} Heap;
+
+void HeapInit(Heap * ph);
+int HIsEmpty(Heap * ph);
+void HInsert(Heap * ph, HData data, Priority pr);
+HData HDelete(Heap * ph);
+
+
+#endif
