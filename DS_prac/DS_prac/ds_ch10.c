40fd005fc2e0b50c48a6b2de9e15317b001cb372
Hyeonseong Jeon <cutz309@gmail.com>
2019-02-01 17:11:40 +0900

    Update

--- DS_prac/DS_prac/ds_ch10.c
+++ DS_prac/DS_prac/ds_ch10.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include "UsefulHeap.h"
 
 int main(void) {
 	//bubble_test();
-	//selection_test();
-	insertion_test();
+	////selection_test();
+	//insertion_test();
+	//heap_test();
+	merge_test();
 	return 0;
 }
 
@@ -52,8 +55,89 @@ void InsertionSort(int arr[], int n) {
 		}
 		arr[j + 1] = insData;
 	}
-
 }
+
+
+int BSearch(int arr[], int first, int last, int target) {
+		int i, j;
+		int half = (first + last) / 2;
+		if (arr[half] == target)
+			return half;
+		else if (arr[half] > target)
+			if (half - 1 - first <= 1)
+				return half;
+			else
+				return BSearch(arr, first, half, target);
+		else if (arr[half] < target)
+			if (last - half + 1 <= 1)
+				return half;
+			return BSearch(arr, half+1, last, target);
+}
+
+void B_InsertionSort(int arr[], int n) {
+	int i, j;
+	int insData, temp;
+	int num;
+
+	for (i = 1; i < n; i++) {
+		insData = arr[i];
+		j = i - 1;
+		num = BSearch(arr, 0, j, insData);
+		arr[num] = insData;
+	}
+}
+
+int PriComp(int n1, int n2) {
+	return n2 - n1;
+}
+
+void HeapSort(int arr[], int n, PriorityComp pc) {
+	Heap heap;
+	int i;
+	HeapInit_u(&heap, pc);
+	for (i = 0; i < n; i++)
+		HInsert_u(&heap, arr[i]);
+	for (i = 0; i < n; i++)
+		arr[i] = HDelete_u(&heap);
+}
+
+void MergeTwoAreas(int arr[], int left, int mid, int right) {
+	int fIdx = left;
+	int rIdx = mid+1;
+	int i;
+	int * sortArr = (int*)malloc(sizeof(int)*(right + 1));
+	int sIdx = left;
+	while (fIdx <= mid && rIdx <= right) {
+		if (arr[fIdx] <= arr[rIdx])
+			sortArr[sIdx] = arr[fIdx++];
+		else
+			sortArr[sIdx] = arr[rIdx++];
+		sIdx++;
+	}
+	if (fIdx > mid) {
+		for (i = rIdx; i <= right; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	else {
+		for (i = fIdx; i <= mid; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	for (i = left; i <= right; i++)
+		arr[i] = sortArr[i];
+	free(sortArr);
+}
+
+void MergeSort(int arr[], int left, int right) {
+	int mid;
+	if (left < right) {
+		mid = (left + right) / 2;
+		MergeSort(arr, left, mid);
+		MergeSort(arr, mid + 1, right);
+		MergeTwoAreas(arr, left, mid, right);
+	}
+	
+}
+
 
 int bubble_test(void) {
 	int arr[4] = { 5,2,1,4 };
@@ -77,8 +161,28 @@ int selection_test(void) {
 int insertion_test(void) {
 	int arr[5] = { 5,2,1,4,3 };
 	int i;
-	InsertionSort(arr, sizeof(arr) / sizeof(int));
+	B_InsertionSort(arr, sizeof(arr) / sizeof(int));
 	for (i = 0; i < 5; i++)
 		printf("%d ", arr[i]);
+	return 0;
+}
+
+int heap_test(void) {
+	int arr[5] = { 5, 2, 1, 4, 3 };
+	int i;
+	HeapSort(arr, sizeof(arr) / sizeof(int), PriComp);
+	for (i = 0; i < 5; i++)
+		printf("%d ", arr[i]);
+	return 0;
+}
+
+int merge_test(void) {
+	int arr[7] = { 3, 2, 4, 1, 7,6, 5 };
+	int i;
+
+	MergeSort(arr, 0, sizeof(arr) / sizeof(int) - 1);
+	for (i = 0; i < 7; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 	return 0;
 }
